5c209f47c6dd4ba4d16f18ba83d96fe34a90d58d
Asa Holland <asaleholland@gmail.com>
2023-05-30 21:11:27 -0700

    feat(Initial Setup): :sparkles: Add initial commands to check and parse diffs

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import * as vscode from 'vscode'; import * as cp from 'child_process';
+const DEFAULT_THRESHOLD = 200;
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pull-request-alert" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pull-request-alert.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Pull Request Alert!');
-	});
-
+	let disposable = vscode.commands.registerCommand('extension.checkDiff', async () => { await checkDiff(); });
 	context.subscriptions.push(disposable);
+	vscode.workspace.onDidSaveTextDocument(async () => { const config = vscode.workspace.getConfiguration('diffChecker'); const autoCheck = config.get<boolean>('autoCheck'); if (autoCheck) { await checkDiff(); } });
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+
+async function checkDiff() {
+	const currentWorkspace = vscode.workspace.workspaceFolders?.[0]; if (!currentWorkspace) { vscode.window.showErrorMessage('No workspace is open'); return; }
+	const currentBranch = await executeCommand('git rev-parse --abbrev-ref HEAD', currentWorkspace.uri.fsPath); const branchPoint = await executeCommand(`git merge-base ${currentBranch.trim()} develop`, currentWorkspace.uri.fsPath); const diff = await executeCommand(`git diff ${branchPoint.trim()}..${currentBranch.trim()} --shortstat`, currentWorkspace.uri.fsPath);
+	const changes = parseDiff(diff); const config = vscode.workspace.getConfiguration('diffChecker');
+
+	const threshold = config.get<number>('threshold') || DEFAULT_THRESHOLD; if (changes >= threshold) { vscode.window.showWarningMessage(`The sum of changes is ${changes}, it's time to submit a PR`); } else { vscode.window.showInformationMessage(`The sum of changes is ${changes}`); }
+}
+function parseDiff(diff: string): number {
+	const matches = diff.match(/(\d+) insertions.*(\d+) deletions/); if (!matches) { return 0; }
+	const insertions = parseInt(matches[1]); const deletions = parseInt(matches[2]); return insertions + deletions;
+}
+
+
+function executeCommand(command: string, cwd: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+		cp.exec(command, { cwd }, (error, stdout, stderr) => {
+			if (error) { reject(error); return; }
+			if (stderr) { reject(stderr); return; }
+			resolve(stdout);
+		});
+	});
+}
+export function deactivate() { }
