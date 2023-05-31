2dd62a239ba77165b7c4c6a9a9304bdac7b535d6
Asa Holland <asaleholland@gmail.com>
2023-05-30 21:26:42 -0700

    feat(Initial Setup): :art: Restructure and add exception handling

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,80 @@
-import * as vscode from 'vscode'; import * as cp from 'child_process';
-const DEFAULT_THRESHOLD = 200;
-export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.checkDiff', async () => { await checkDiff(); });
-	context.subscriptions.push(disposable);
-	vscode.workspace.onDidSaveTextDocument(async () => { const config = vscode.workspace.getConfiguration('diffChecker'); const autoCheck = config.get<boolean>('autoCheck'); if (autoCheck) { await checkDiff(); } });
-}
+import * as vscode from 'vscode';
+import * as cp from 'child_process';
 
+const DEFAULT_THRESHOLD = 200;
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposable = vscode.commands.registerCommand('extension.checkDiff', async () => {
+		await checkDiff();
+	});
+
+	context.subscriptions.push(disposable);
+
+	vscode.workspace.onDidSaveTextDocument(async () => {
+		const config = vscode.workspace.getConfiguration('diffChecker');
+		const autoCheck = config.get<boolean>('autoCheck');
+
+		if (autoCheck) {
+			await checkDiff();
+		}
+	});
+}
 
 async function checkDiff() {
-	const currentWorkspace = vscode.workspace.workspaceFolders?.[0]; if (!currentWorkspace) { vscode.window.showErrorMessage('No workspace is open'); return; }
-	const currentBranch = await executeCommand('git rev-parse --abbrev-ref HEAD', currentWorkspace.uri.fsPath); const branchPoint = await executeCommand(`git merge-base ${currentBranch.trim()} develop`, currentWorkspace.uri.fsPath); const diff = await executeCommand(`git diff ${branchPoint.trim()}..${currentBranch.trim()} --shortstat`, currentWorkspace.uri.fsPath);
-	const changes = parseDiff(diff); const config = vscode.workspace.getConfiguration('diffChecker');
+	const currentWorkspace = vscode.workspace.workspaceFolders?.[0];
 
-	const threshold = config.get<number>('threshold') || DEFAULT_THRESHOLD; if (changes >= threshold) { vscode.window.showWarningMessage(`The sum of changes is ${changes}, it's time to submit a PR`); } else { vscode.window.showInformationMessage(`The sum of changes is ${changes}`); }
+	if (!currentWorkspace) {
+		vscode.window.showErrorMessage('No workspace is open');
+		return;
+	}
+
+	try {
+		const currentBranch = await executeCommand('git rev-parse --abbrev-ref HEAD', currentWorkspace.uri.fsPath);
+		const branchPoint = await executeCommand(`git merge-base ${currentBranch.trim()} develop`, currentWorkspace.uri.fsPath);
+		const diff = await executeCommand(`git diff ${branchPoint.trim()}..${currentBranch.trim()} --shortstat`, currentWorkspace.uri.fsPath);
+
+		const changes = parseDiff(diff);
+		const config = vscode.workspace.getConfiguration('diffChecker');
+		const threshold = config.get<number>('threshold') || DEFAULT_THRESHOLD;
+
+		if (changes >= threshold) {
+			vscode.window.showWarningMessage(`The sum of changes is ${changes}, it's time to submit a PR`);
+		} else {
+			vscode.window.showInformationMessage(`The sum of changes is ${changes}`);
+		}
+
+	} catch (err) {
+		vscode.window.showErrorMessage(err.message);
+	}
 }
+
 function parseDiff(diff: string): number {
-	const matches = diff.match(/(\d+) insertions.*(\d+) deletions/); if (!matches) { return 0; }
-	const insertions = parseInt(matches[1]); const deletions = parseInt(matches[2]); return insertions + deletions;
-}
+	const matches = diff.match(/(\\d+) insertions.*(\\d+) deletions/);
+	if (!matches) {
+		return 0;
+	}
 
+	const insertions = parseInt(matches[1]);
+	const deletions = parseInt(matches[2]);
+	return insertions + deletions;
+}
 
 function executeCommand(command: string, cwd: string): Promise<string> {
 	return new Promise((resolve, reject) => {
 		cp.exec(command, { cwd }, (error, stdout, stderr) => {
-			if (error) { reject(error); return; }
-			if (stderr) { reject(stderr); return; }
+			if (error) {
+				reject(error);
+				return;
+			}
+
+			if (stderr) {
+				reject(new Error(stderr));
+				return;
+			}
+
 			resolve(stdout);
 		});
 	});
 }
+
 export function deactivate() { }
