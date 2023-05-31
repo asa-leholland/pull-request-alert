433e96e7bd681b5d1af34f34bb87ffed0a1b641b
Asa Holland <asaleholland@gmail.com>
2023-05-30 23:31:15 -0700

    fix(Initial Setup): :bug: Resolve issue with non-branch repo

--- src/extension.ts
+++ src/extension.ts
@@ -30,17 +30,24 @@ async function checkDiff() {
 
 	try {
 		const currentBranch = await executeCommand('git rev-parse --abbrev-ref HEAD', currentWorkspace.uri.fsPath);
-		const branchPoint = await executeCommand(`git merge-base ${currentBranch.trim()} develop`, currentWorkspace.uri.fsPath);
+		const defaultBranch = await executeCommand('git remote show origin | awk \'/HEAD branch/ {print $NF}\'', currentWorkspace.uri.fsPath);
+		console.log(`Current Branch: ${currentBranch}`);
+		console.log(`Default Branch: ${defaultBranch}`);
+		const branchPoint = await executeCommand(`git merge-base ${currentBranch.trim()} ${defaultBranch.trim()}`, currentWorkspace.uri.fsPath);
+		console.log(`Branch Point: ${branchPoint}`);
 		const diff = await executeCommand(`git diff ${branchPoint.trim()}..${currentBranch.trim()} --shortstat`, currentWorkspace.uri.fsPath);
+		console.log(`Diff: ${diff}`);
 
 		const changes = parseDiff(diff);
 		const config = vscode.workspace.getConfiguration('pullRequestAlert');
 		const threshold = config.get<number>('threshold') || DEFAULT_THRESHOLD;
 
-		if (changes >= threshold) {
-			vscode.window.showWarningMessage(`The sum of changes is ${changes}, it's time to submit a PR`);
+		if (changes >= threshold && currentBranch.trim() !== defaultBranch.trim()) {
+			vscode.window.showWarningMessage(`The sum of changes is ${changes}. It's time to submit a pull request.`);
+		} else if (changes >= threshold && currentBranch.trim() === defaultBranch.trim()) {
+			vscode.window.showWarningMessage(`The sum of changes is ${changes}. It's time to submit a pull request to merge your changes.`);
 		} else {
-			vscode.window.showInformationMessage(`The sum of changes is ${changes}`);
+			vscode.window.showInformationMessage(`The sum of changes is ${changes}.`);
 		}
 
 	} catch (err: any) {
@@ -72,6 +79,8 @@ function executeCommand(command: string, cwd: string): Promise<string> {
 				return;
 			}
 
+			console.log(`Command: ${command}`);
+			console.log(`Output: ${stdout}`);
 			resolve(stdout);
 		});
 	});
