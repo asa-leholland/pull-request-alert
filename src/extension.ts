0bb570f4668103d1e7c0bc3fad9ad0988b2683a0
Asa Holland <asaleholland@gmail.com>
2023-06-10 11:13:41 -0700

    fix(Calculate lines added): :fire: Delete unused code

--- src/extension.ts
+++ src/extension.ts
@@ -25,28 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 }
 
-async function calculateDeletedLines(): Promise<number> {
-	const currentWorkspace = vscode.workspace.workspaceFolders?.[0];
-
-	if (!currentWorkspace) {
-		vscode.window.showErrorMessage('No workspace is open. Please open a workspace to run the Pull Request Alert extension.');
-		return 0;
-	}
-
-    let numDeletedLines = 0;
-    try {
-        const numDeletedLinesCommand = 'git diff --cached --numstat | findstr /R /C:"^[0-9]*[[:space:]]+[0-9]*[[:space:]]" | for /F "tokens=2" %a in (\'findstr /R /C:"^[0-9]*[[:space:]]+[0-9]*[[:space:]]"\') do set /a s+=%a';
-        numDeletedLines = parseInt(await executeCommand(numDeletedLinesCommand, currentWorkspace.uri.fsPath));
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        outputChannel.appendLine(`Error: ${error}`);
-    }
-
-    console.log(`Number of deleted lines: ${numDeletedLines}`);
-    outputChannel.appendLine(`Number of deleted lines: ${numDeletedLines}`);
-
-    return numDeletedLines;
-}
 
 async function checkDiff() {
 	const currentWorkspace = vscode.workspace.workspaceFolders?.[0];
@@ -63,10 +41,8 @@ async function checkDiff() {
 
 		let numStagedLinesCommand = '';
 		if (process.platform === 'win32') {
-			// console.log('Windows');
 			numStagedLinesCommand = 'git diff --numstat | findstr /R /C:"^[0-9]*" | for /F "tokens=1" %a in (\'findstr /R /C:"^[0-9]*"\') do set /a s+=%a';
 		} else {
-			// console.log('Not Windows');
 
 			numStagedLinesCommand = 'git diff --cached --numstat | grep -E "^[0-9]+" | cut -f1 | paste -sd+ - | bc';
 		}
@@ -102,32 +78,5 @@ async function executeCommandToInt(command: string, cwd: string): Promise<number
 	});
   }
 
-interface DiffStats {
-	total: number;
-	additions: number;
-	modifications: number;
-	deletions: number;
-}
-
-
-function executeCommand(command: string, cwd: string): Promise<string> {
-	return new Promise((resolve, reject) => {
-		cp.exec(command, { cwd }, (error, stdout, stderr) => {
-			if (error) {
-				reject(error);
-				return;
-			}
-
-			if (stderr) {
-				reject(new Error(stderr));
-				return;
-			}
-
-			console.log(`Command: ${command}`);
-			console.log(`Output: ${stdout}`);
-			resolve(stdout);
-		});
-	});
-}
 
 export function deactivate() { }
